769604b57badf5073068624a64c1b5e582e4333a
cautree <yanyanliujs@gmail.com>
2024-10-13 09:40:02 -0400

    update

--- data_housekeeping/bash_code/rm_temp_work_5.sh
+++ data_housekeeping/bash_code/rm_temp_work_5.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+date_range=202410
+
 today=$(date +%Y%m%d)
 yesterday=$(date -r $(( $(date '+%s') -   86400 )) '+%Y%m%d')
 
 #######################
 #clean up the fastq bucket
 #######################
-aws s3 ls s3://seqwell-fastq/ | grep -E '2024' | cut -c32- | sed 's|/||g' > run_name
+aws s3 ls s3://seqwell-fastq/ | grep -E $date_range | cut -c32- | sed 's|/||g' > run_name
 cat run_name | cut -c1-8 > date
 
 paste date run_name > run_info
@@ -23,7 +25,7 @@ rm run_name date run_info run_analysis_work_to_rm
 #clean up the analysis bucket
 #######################
 
-aws s3 ls s3://seqwell-analysis/ | grep -E '2024' | cut -c32- | sed 's|/||g' > run_name
+aws s3 ls s3://seqwell-analysis/ | grep -E $date_range | cut -c32- | sed 's|/||g' > run_name
 cat run_name | cut -c1-8 > date
 
 paste date run_name > run_info
