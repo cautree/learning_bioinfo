36dff57a4c169bc6efd8417043db3f899279e219
cautree <yanyanliujs@gmail.com>
2024-10-13 09:36:35 -0400

    add rm work

---
+++ data_housekeeping/bash_code/rm_temp_work_5.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+today=$(date +%Y%m%d)
+yesterday=$(date -r $(( $(date '+%s') -   86400 )) '+%Y%m%d')
+
+#######################
+#clean up the fastq bucket
+#######################
+aws s3 ls s3://seqwell-fastq/ | grep -E '2024' | cut -c32- | sed 's|/||g' > run_name
+cat run_name | cut -c1-8 > date
+
+paste date run_name > run_info
+
+cat run_info | awk -v var=$yesterday '{ if( $1< var) print $2}' > run_analysis_work_to_rm
+
+while read line; do
+aws s3 rm  s3://seqwell-fastq/$line/work/ --recursive 
+done < run_analysis_work_to_rm
+
+rm run_name date run_info run_analysis_work_to_rm
+
+#######################
+#clean up the analysis bucket
+#######################
+
+aws s3 ls s3://seqwell-analysis/ | grep -E '2024' | cut -c32- | sed 's|/||g' > run_name
+cat run_name | cut -c1-8 > date
+
+paste date run_name > run_info
+
+cat run_info | awk -v var=$yesterday '{ if( $1< var) print $2}' > run_fq_work_to_rm
+
+while read line; do
+aws s3 rm s3://seqwell-analysis/$line/ --recursive --exclude="*" --include="*/work/*" 
+done < run_fq_work_to_rm
+
+
+
+
