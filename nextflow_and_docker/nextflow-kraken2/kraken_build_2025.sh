cdc2e6a50cfe5b9cc57fbec5011a7455b4567c07
cautree <yanyanliujs@gmail.com>
2025-03-03 22:22:50 -0500

    kraken build

--- nextflow_and_docker/nextflow-kraken2/kraken_build_2025.sh
+++ nextflow_and_docker/nextflow-kraken2/kraken_build_2025.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_08gb_20230605.tar.gz
+tar -xvzf k2_standard_08gb_20230605.tar.gz -C /home/ec2-user/kraken/kraken2-bacteria-db/
+
+
+##did not work
+
 mkdir -p /home/ec2-user/kraken/k2-bacteria-db/
 rsync --copy-links --recursive --times --verbose rsync://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/ /home/ec2-user/kraken/k2-bacteria-db/library/bacteria/
 kraken2-build --download-taxonomy --db /home/ec2-user/kraken/k2-bacteria-db/
