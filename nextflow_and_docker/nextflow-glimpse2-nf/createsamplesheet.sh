e01038187debf5bb9945a95f0bdc3fa23fd5b803
cautree <yanyanliujs@gmail.com>
2025-03-11 10:39:07 -0400

    add createsamplesheet

---
+++ nextflow_and_docker/nextflow-glimpse2-nf/createsamplesheet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+path=$1
+bam_folder_path=${path}/bam
+
+ls $bam_folder_path | grep bam | grep bai > index_list
+ls $bam_folder_path | grep bam |  grep -v bai > bam_list
+
+
+cat bam_list | sed -E 's/\.bam$//; s/[-_.]//g'  > sample_id
+
+while read index; do
+echo ${bam_folder_path}/$index >> index_column
+done < index_list
+
+while read bam; do
+echo ${bam_folder_path}/$bam >> bam_column
+done < bam_list
+
+
+
+echo sample,file,index > sample_bam_header
+paste sample_id bam_column index_column | tr '\t' ',' >sample_bam_content
+cat sample_bam_header sample_bam_content | grep -v bamlist > sample_bam.csv
+rm bam_column index_column sample_bam_header sample_bam_content bam_list index_list sample_id
+cat sample_bam.csv
+
